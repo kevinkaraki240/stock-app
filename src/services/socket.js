f3b9f0545cfc300360bbb803ec5381b860b16192
Dang Phuong <dangphuong2400@gmail.com>
2026-09-15 15:12:27 +0700

    Use env var for API URL

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const SOCKET_URL = "http://localhost:4000"
+const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:4000"
 
 // Tạo 1 instance duy nhất, dùng chung cho cả app thay vì mỗi component tự connect
 export const socket = io(SOCKET_URL, {
